70d66fa148aee86013a9ec3cda0070364768dc9f
Ubuntu <Emerickd9@projectvm.tqyejhsckjtublsyw3o5xxtchd.gx.internal.cloudapp.net>
2022-09-06 23:10:26 +0000

    all done

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=08138114703/projectml
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run projectml\
+	--image=$dockerpath\
+	--port=80 --labels app=projectml
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward projectml 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=08138114703/projectml
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker --tag projectml $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
